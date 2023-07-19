4b167f381da861d4a32475a961b767763f394d58
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-07-19 19:14:06 +0700

    feat: separate light to other script

--- Assets/_Scripts/Characters/BaseWorldCharacter.cs
+++ Assets/_Scripts/Characters/BaseWorldCharacter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,24 +10,19 @@ public class BaseWorldCharacter : BaseGridXYGameObject
 {
     public float MoveSpeed = 5f;
 
-    private Rigidbody2D _rb;
-
-    [Header("Lights Components")] 
-    [SerializeField] private Light2D _light2D;
-    [SerializeField] private float _maxRange = 5f;
-    [SerializeField] private int _rayCount = 36;
-    [SerializeField] private LayerMask _wallLayerMask;
-
-
+    protected Rigidbody2D Rb;
+    protected CharacterLight CharacterLight;
+    
     protected override void Start()
     {
         base.Start();
-        _rb = GetComponent<Rigidbody2D>();
+        Rb = GetComponent<Rigidbody2D>();
+        CharacterLight = GetComponent<CharacterLight>();
     }
 
     private void Update()
     {
-        UpdateLight();
+        
     }
 
     private void Move()
@@ -37,51 +33,9 @@ public class BaseWorldCharacter : BaseGridXYGameObject
         Vector2 movement = new Vector2(horizontalInput, verticalInput);
         movement.Normalize();
 
-        _rb.AddForce(movement * MoveSpeed);
+        Rb.AddForce(movement * MoveSpeed);
         
         //Debug.Log(GetCell().XIndex + " " + GetCell().YIndex);
-    }
-
-    private void UpdateLight()
-    {
-        
-        // Calculate the angle between each ray
-        float angleStep = 360f / _rayCount;
-
-        // Create a new shape for the light
-        var shapePath = new Vector3[_rayCount];
-
-        // For the light not straight the mesh is not linear
-        Vector3 zigzagOffset = new Vector3(0.01f, 0.01f);
-        
-        for (int i = 0; i < _rayCount; i++)
-        {
-            // Calculate the direction of the ray
-            float angle = i * angleStep;
-            Vector3 direction = Quaternion.Euler(0f, 0f, angle) * Vector3.right;
-            
-            
-            // Perform the raycast
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, _maxRange, _wallLayerMask);
-
-            // Check if the ray hit a wall
-            if (hit.collider != null)
-            {
-                // Set the vertex position to the collision point
-                shapePath[i] = (Vector3)hit.point - transform.position + zigzagOffset;
-            }
-            else
-            {
-                // Set the vertex position to the end of the ray
-                shapePath[i] = direction * _maxRange + zigzagOffset;
-            }
-
-            zigzagOffset = -zigzagOffset;
-            //Debug.Log($"Shape path {i} = {shapePath[i]}");
-        }
-
-        // Update the light's shape with the new path
-        _light2D.SetShapePath(shapePath);
     }
 
 }
