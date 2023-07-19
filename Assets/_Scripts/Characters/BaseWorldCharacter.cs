530e9e186e65bfd4401a286b393bca275901a55d
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-07-19 15:23:23 +0700

    test: lighting

--- Assets/_Scripts/Characters/BaseWorldCharacter.cs
+++ Assets/_Scripts/Characters/BaseWorldCharacter.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class BaseWorldCharacter : BaseGridXYGameObject
 {
     public float MoveSpeed = 5f;
+
     private Rigidbody2D _rb;
+    
+    [Header("Lights Components")] 
+    [SerializeField] private Light2D _light2D;
+    [SerializeField] private float _maxRange = 5f;
+    [SerializeField] private int _rayCount = 36;
+    [SerializeField] private LayerMask _wallLayerMask;
+
 
     protected override void Start()
     {
@@ -14,6 +23,11 @@ public class BaseWorldCharacter : BaseGridXYGameObject
     }
 
     private void Update()
+    {
+        UpdateLight();
+    }
+
+    private void Move()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
@@ -26,8 +40,41 @@ public class BaseWorldCharacter : BaseGridXYGameObject
         //Debug.Log(GetCell().XIndex + " " + GetCell().YIndex);
     }
 
-    private void MoveToCell()
+    private void UpdateLight()
     {
         
+        // Calculate the angle between each ray
+        float angleStep = 360f / _rayCount;
+
+        // Create a new shape for the light
+        var shapePath = new Vector3[_rayCount];
+
+        for (int i = 0; i < _rayCount; i++)
+        {
+            // Calculate the direction of the ray
+            float angle = i * angleStep;
+            Vector3 direction = Quaternion.Euler(0f, 0f, angle) * Vector3.right;
+
+            // Perform the raycast
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, _maxRange, _wallLayerMask);
+
+            // Check if the ray hit a wall
+            if (hit.collider != null)
+            {
+                // Set the vertex position to the collision point
+                shapePath[i] = (Vector3)hit.point - transform.position;
+            }
+            else
+            {
+                // Set the vertex position to the end of the ray
+                shapePath[i] = direction * _maxRange;
+            }
+            
+            Debug.Log($"Shape path {i} = {shapePath[i]}");
+        }
+
+        // Update the light's shape with the new path
+        _light2D.SetShapePath(shapePath);
     }
+
 }
