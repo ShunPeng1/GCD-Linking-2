e7f55670abf3620f2a9a3dcea3df9c6e860ca62f
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-07-19 18:54:06 +0700

    fix: light bug

--- Assets/_Scripts/Characters/BaseWorldCharacter.cs
+++ Assets/_Scripts/Characters/BaseWorldCharacter.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
+
+
 public class BaseWorldCharacter : BaseGridXYGameObject
 {
     public float MoveSpeed = 5f;
 
     private Rigidbody2D _rb;
-    
+
     [Header("Lights Components")] 
     [SerializeField] private Light2D _light2D;
     [SerializeField] private float _maxRange = 5f;
@@ -49,12 +51,16 @@ public class BaseWorldCharacter : BaseGridXYGameObject
         // Create a new shape for the light
         var shapePath = new Vector3[_rayCount];
 
+        // For the light not straight the mesh is not linear
+        Vector3 zigzagOffset = new Vector3(0.01f, 0.01f);
+        
         for (int i = 0; i < _rayCount; i++)
         {
             // Calculate the direction of the ray
             float angle = i * angleStep;
             Vector3 direction = Quaternion.Euler(0f, 0f, angle) * Vector3.right;
-
+            
+            
             // Perform the raycast
             RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, _maxRange, _wallLayerMask);
 
@@ -62,15 +68,16 @@ public class BaseWorldCharacter : BaseGridXYGameObject
             if (hit.collider != null)
             {
                 // Set the vertex position to the collision point
-                shapePath[i] = (Vector3)hit.point - transform.position;
+                shapePath[i] = (Vector3)hit.point - transform.position + zigzagOffset;
             }
             else
             {
                 // Set the vertex position to the end of the ray
-                shapePath[i] = direction * _maxRange;
+                shapePath[i] = direction * _maxRange + zigzagOffset;
             }
-            
-            Debug.Log($"Shape path {i} = {shapePath[i]}");
+
+            zigzagOffset = -zigzagOffset;
+            //Debug.Log($"Shape path {i} = {shapePath[i]}");
         }
 
         // Update the light's shape with the new path
