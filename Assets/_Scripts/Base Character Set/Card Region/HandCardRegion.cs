340adfe8d52495579f3bdc5d1a7325fd834c4c47
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-07-25 22:49:56 +0700

    feat: hand disable animation

--- Assets/_Scripts/Base Character Set/Card Region/HandCardRegion.cs
+++ Assets/_Scripts/Base Character Set/Card Region/HandCardRegion.cs
@@ -1,9 +1,23 @@
-﻿using Shun_Card_System;
+﻿using System;
+using DG.Tweening;
+using Shun_Card_System;
+using UnityEngine;
 
 namespace _Scripts.Cards.Card_UI
 {
     public class HandCardRegion : BaseCardRegion
     {
+        [SerializeField] private Vector3 _moveInteractableOffset = new Vector3(0,-2, 0);
+        [SerializeField] private float _moveInteractableDuration = 0.25f;
+        [SerializeField] private Ease _moveInteractableEase = Ease.OutCubic;
+        private Vector3 _destinationPosition;
+
+
+        private void Start()
+        {
+            _destinationPosition = transform.localPosition;
+        }
+
         protected override void OnSuccessfullyAddCard(BaseCardGameObject baseCardGameObject, BaseCardHolder baseCardHolder)
         {
             var characterCardGameObject = (BaseCharacterCardGameObject) baseCardGameObject;
@@ -16,9 +30,24 @@ namespace _Scripts.Cards.Card_UI
 
         protected override void OnSuccessfullyRemoveCard(BaseCardGameObject baseCardGameObject, BaseCardHolder baseCardHolder)
         {
-            var characterCardGameObject = (BaseCharacterCardGameObject) baseCardGameObject;
+            //var characterCardGameObject = (BaseCharacterCardGameObject) baseCardGameObject;
             
 
+        }
+
+        public override void EnableInteractable()
+        {
+            base.EnableInteractable();
+            _destinationPosition += _moveInteractableOffset; 
+            transform.DOLocalMove(_destinationPosition, _moveInteractableDuration).SetEase(_moveInteractableEase);
+
+        }
+
+        public override void DisableInteractable()
+        {
+            base.DisableInteractable();
+            _destinationPosition -= _moveInteractableOffset; 
+            transform.DOLocalMove(_destinationPosition, _moveInteractableDuration).SetEase(_moveInteractableEase);
         }
     }
 }
